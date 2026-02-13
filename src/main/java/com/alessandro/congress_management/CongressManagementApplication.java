1291830267e1206b79c55597edae3100dbafe8b0
Rudy Reyes <rudyreyes202031213@cunoc.edu.gt>
2026-02-13 09:39:58 -0600

    add: initializing the project

---
+++ src/main/java/com/alessandro/congress_management/CongressManagementApplication.java
@@ -0,0 +1,13 @@
+package com.alessandro.congress_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongressManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CongressManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alessandro/congress_management/CongressManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alessandro.congress_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongressManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
